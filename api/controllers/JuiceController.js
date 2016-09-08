05e9cd84f304c5bcf5bd0ee98961ee2ef22a687c
Stuart Filar <stuart.filar@gmail.com>
2016-09-08 09:53:41 -0400

    initial commit

---
+++ api/controllers/JuiceController.js
@@ -0,0 +1,11 @@
+/**
+ * JuiceController
+ *
+ * @description :: Server-side logic for managing juices
+ * @help        :: See http://sailsjs.org/#!/documentation/concepts/Controllers
+ */
+
+module.exports = {
+	
+};
+
